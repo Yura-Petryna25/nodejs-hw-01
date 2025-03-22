caac151ca4c3c207fa2b08fe36ec093afec1db4b
Yura Petryna <yur4ik911@gmail.com>
2025-03-22 10:01:05 +0200

    start node

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = async (updatedContacts) => {};
+import fs from 'node:fs/promises';
+export const writeContacts = async (updatedContacts) => {
+  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+};
